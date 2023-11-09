7a3c3a6aec22caaadad1da416bbd9bdd149e05b9
Vilberto Junior <149752352+Vilbj1@users.noreply.github.com>
2023-11-09 20:14:07 -0300

    Add files via upload

---
+++ EntradaSaída/EntradaSaída/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaída
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Maria e ");
+            Console.WriteLine("Joao");
+
+            //int letra = Console.Read();
+            string texto = Console.ReadLine();
+
+            Console.WriteLine(texto);
+            
+            Console.ReadKey();
+        }
+    }
+}
